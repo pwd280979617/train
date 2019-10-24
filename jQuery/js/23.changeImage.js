7af433341c9ae1f290eca95255b337836415beaa
pwd280979617 <280979617@qq.com>
2019-10-24 11:18:18 +0800

    0.1.8

---
+++ jQuery/js/23.changeImage.js
@@ -0,0 +1,43 @@
+$(function(){
+    var $div1 = $('#div1')
+    var $div2 = $('#div2')
+    var $btn1 = $('#btn1')
+    var $btn2 = $('#btn2')
+    var $box1 = $('#box1')
+    var pageTime = 400 //总的翻页时间
+    var itemTime = 30 //单位翻页时间
+    var offsetLeft = 600 //图片宽度
+    
+
+    //1.点击向左向右按钮平滑切换图片
+    //2.无线循环切换,第一页的上一页为最后一页,最后一页的下一页为第一页
+    //3.每3秒自动切换到下一页
+    //4.当鼠标进入图片区域是,图片切换自动停止,当鼠标离开是开始切换
+    //5.切换页面时下面的点同步切换
+    //6.点击点图标切换到对应的图片
+
+    //1.点击向左向右按钮平滑切换图片
+    $btn1.click(function(){
+        pageNext(false);
+    })
+    
+    $btn2.click(function(){
+        pageNext(true);
+    })
+    
+    function pageNext (next){
+        var currentLeft  = $div2.position().left //获取当前偏移量
+        var offset =  next ? -offsetLeft : offsetLeft //判断向左向右
+        var targetLeft = currentLeft + offset //计算移动目标偏移量
+        var itemLeft = offset/(pageTime/itemTime) //计算单位便宜距离
+        var  timer = setInterval(function(){
+            currentLeft = currentLeft+itemLeft
+            if(next ? currentLeft <= targetLeft : currentLeft >= targetLeft){
+                clearInterval(timer)
+                currentLeft = targetLeft
+            }
+            $div2.css('left',currentLeft)
+        },itemTime)
+    }
+    
+})
