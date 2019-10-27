948e6f8d3b74602ee28d56f2343dad1f24c03f84
pwd280979617 <280979617@qq.com>
2019-10-27 14:43:06 +0800

    0.2.0

--- jQuery/js/23.changeImage.js
+++ jQuery/js/23.changeImage.js
@@ -4,13 +4,17 @@ $(function(){
     var $btn1 = $('#btn1')
     var $btn2 = $('#btn2')
     var $box1 = $('#box1')
+    var $points = $('a')
     var pageTime = 400 //总的翻页时间
     var itemTime = 30 //单位翻页时间
     var offsetLeft = 600 //图片宽度
     var imgNumber = $('#div2>img').length
+    var boxNumber = $points.length
+    var index = 0 //图片的下标
+    var moving = false //表示翻页的状态
     
 
-    //1.点击向左向右按钮平滑切换图片
+    //1.点击向左向右按钮平滑切换图  片
     //2.无线循环切换,第一页的上一页为最后一页,最后一页的下一页为第一页
     //3.每3秒自动切换到下一页
     //4.当鼠标进入图片区域是,图片切换自动停止,当鼠标离开是开始切换
@@ -29,14 +33,24 @@ $(function(){
 
     //2.无线循环切换,第一页的上一页为最后一页,最后一页的下一页为第一页
     function pageNext (next){
+        if(moving){
+            return
+        }
+        moving = true;
+        var offset = 0
         var currentLeft  = $div2.position().left //获取当前偏移量
-        var offset =  next ? -offsetLeft : offsetLeft //判断向左向右
+        if(typeof next == 'boolean'){
+            offset =  next ? -offsetLeft : offsetLeft //判断向左向右
+        }else {
+            offset = -(next - index)*offsetLeft
+        }
         var targetLeft = currentLeft + offset //计算移动目标偏移量
         var itemLeft = offset/(pageTime/itemTime) //计算单位便宜距离
         var  timer = setInterval(function(){
             currentLeft = currentLeft+itemLeft
             if(next ? currentLeft <= targetLeft : currentLeft >= targetLeft){
                 clearInterval(timer)
+                moving = false
                 currentLeft = targetLeft
                 if(currentLeft === -(imgNumber-1)*offsetLeft){
                     currentLeft = -offsetLeft
@@ -46,6 +60,32 @@ $(function(){
             }
             $div2.css('left',currentLeft)
         },itemTime)
+        updateBox1(next);
+    }
+
+    //5.切换页面时下面的点同步切换
+    function updateBox1 (next){
+        var targetIndex = 0 //原点的下标
+        if(typeof next == 'boolean'){
+            if(next){
+                targetIndex = index + 1;
+                if(targetIndex === boxNumber){
+                    targetIndex = 0;
+                }
+            } else {
+                targetIndex = index -1;
+                if(targetIndex === -1){
+                    targetIndex = boxNumber-1
+                }
+            }
+        }else{
+            targetIndex = next
+        }
+        
+        $points.eq(index).removeClass("on")
+        $points.eq(targetIndex).addClass("on")
+        index = targetIndex;
+
     }
     
     //3.每3秒自动切换到下一页
@@ -59,7 +99,15 @@ $(function(){
     },function(){
         timer1 = setInterval(function(){
             pageNext(true);
-        },3000)
+        },1000)
+    })
+
+    //6.点击点图标切换到对应的图片
+    $points.click(function(){
+        var targetIndex = $(this).index()
+        if(targetIndex !== index){
+            pageNext(targetIndex)
+        }
     })
 
 })
