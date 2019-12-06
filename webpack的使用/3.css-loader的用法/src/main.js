74594342691d61174296ebf7ac48e68867810015
pwd280979617 <280979617@qq.com>
2019-12-06 14:16:08 +0800

    0.3.26

---
+++ webpack的使用/3.css-loader的用法/src/main.js
@@ -0,0 +1,9 @@
+const {math1,math2} = require('./js/methods.js')
+
+import {book1} from  './js/data.js'
+
+console.log(math1(1,2))
+console.log(math2(2,2))
+console.log(book1)
+
+import css from './css/style.css';
